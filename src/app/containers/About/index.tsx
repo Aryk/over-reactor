35192143aa51cfe85be44fe1c42655310e6aefaa
Aryk Grosz <aryk.grosz@gmail.com>
2016-11-15 04:51:47 +0200

    Add Helmet overwrite example

--- src/app/containers/About/index.tsx
+++ src/app/containers/About/index.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import * as Helmet from 'react-helmet';
 const s = require('./style.css');
 
 class About extends React.Component<any, any> {
   public render() {
     return (
       <div className={s.about}>
+        <Helmet
+          title="About"
+          meta={[{property: 'og:title', content: 'About'}]} />
         <h4>About</h4>
       </div>
     );
